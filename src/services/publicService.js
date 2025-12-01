b4dc460c8c78e549bcf58409c86ec997d18584ce
Murat Ertik <dmuratertik1@gmail.com>
2025-12-01 06:09:01 -0800

    public service has been created.

---
+++ src/services/publicService.js
@@ -0,0 +1,23 @@
+import api from './api';
+
+const getAirports = async () => {
+  const response = await api.get('/public/airports');
+  return response.data;
+};
+
+const searchFlights = async (fromId, toId, date) => {
+  // Query parametreleri ile arama
+  const response = await api.get(`/public/flights/search?from=${fromId}&to=${toId}&date=${date}`);
+  return response.data;
+};
+
+const getFlightSeats = async (flightId) => {
+  const response = await api.get(`/public/flights/${flightId}/seats`);
+  return response.data;
+};
+
+export default {
+  getAirports,
+  searchFlights,
+  getFlightSeats
+};
