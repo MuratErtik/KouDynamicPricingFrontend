5456a07a83be176c16ef32517a958382ebb22693
Murat Ertik <dmuratertik1@gmail.com>
2025-12-13 19:40:25 +0300

    findong with pnr service has been added.

---
+++ src/services/ticketService.js
@@ -0,0 +1,15 @@
+import api from './api';
+
+const ticketService = {
+  // PNR ve TC No ile bilet sorgulama
+  searchByPnr: async (pnr, identityNumber) => {
+    // Backend @PostMapping bekliyor varsayımıyla:
+    const response = await api.post('public/tickets/searchFlightInfo', {
+      pnr: pnr,
+      identityNumber: identityNumber
+    });
+    return response.data;
+  }
+};
+
+export default ticketService;
